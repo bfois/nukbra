52bcf42df6f7d15939cf7b2146c51af979a7407d
BraianF <97342723+bfois@users.noreply.github.com>
2022-09-21 19:23:31 -0300

    ashe1
    1

--- battle.js
+++ battle.js
@@ -68,7 +68,6 @@ const xhr = new XMLHttpRequest();
 function onRequestHanler(){
     if(this.readyState==4 && this.status==200){
         const data = JSON.parse(this.response);
-        console.log(data)
         const HTMLResponse = document.querySelector("#copas")
 
         const check = data.brawlers
